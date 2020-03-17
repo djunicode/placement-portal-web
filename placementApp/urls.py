b7150fcf16d3706a2b7b350bfc399d13e671e174
SakshiUppoor <sakshiuppoor@gmail.com>
2020-03-17 22:04:33 +0530

    Add GET Student &  Position endpoints + Excel file generation

---
+++ placementApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register("students", views.StudentViewSet)
+router.register("positions", views.PositionViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("get_xls/<int:company_id>", views.get_xls, name="get_xls"),
+]
--- placementPortal/urls.py
+++ placementPortal/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import url
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("djoser.urls.authtoken")),
+    path("", include("placementApp.urls")),
 ]
