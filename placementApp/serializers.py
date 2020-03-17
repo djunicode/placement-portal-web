9d20278d1dac5c5fe609532775248a213873adb8
SakshiUppoor <sakshiuppoor@gmail.com>
2020-03-17 22:02:32 +0530

    Create Student and Position serializers

---
+++ placementApp/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Student, Position, Company
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = (
+            "id",
+            "username",
+            # "first_name",
+            # "last_name",
+            "email",
+            "sap_ID",
+            "department",
+            "year",
+        )
+
+
+class PositionSerializer(serializers.ModelSerializer):
+    company = serializers.PrimaryKeyRelatedField(queryset=Company.objects.all())
+
+    class Meta:
+        model = Position
+        fields = "__all__"
+
