8de2b83c3a047ec4f5648fad91f1105dd9acf77c
SakshiUppoor <sakshiuppoor@gmail.com>
2020-04-02 20:23:32 +0530

    Fix AnonymousUser error

--- placementApp/permissions.py
+++ placementApp/permissions.py
@@ -7,7 +7,7 @@ class IsTPOOrOwner(BasePermission):
     message = "You do not have the permission to perform this action."
 
     def has_permission(self, request, view):
-        if request.user.is_student() and view.action == "list":
+        if request.user.is_authenticated and  (request.user.is_student() and view.action == "list"):
             return False
         return request.user.is_authenticated
 
@@ -20,13 +20,13 @@ class IsTPOOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
         if view.action == "create":
-            return request.user.is_tpo()
+            return request.user.is_authenticated and request.user.is_tpo()
         return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         if view.action == "retrieve":
             return True
-        return request.user.is_tpo()
+        return request.user.is_authenticated and request.user.is_tpo()
 
 
 class IsStaff(BasePermission):
@@ -55,7 +55,7 @@ class ApplicationPermissions(BasePermission):
             return request.user.is_authenticated
             # All authenticated users can Retrieve applications
 
-        return request.user.is_tpo()
+        return request.user.is_authenticated and request.user.is_tpo()
         # Only TPO can update applications
 
 
