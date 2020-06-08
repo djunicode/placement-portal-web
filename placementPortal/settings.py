76cc2c7cb07958b748b2eca289c0a3ee821a661b
kanishkshah <shahkanishk99@gmail.com>
2020-06-08 17:34:08 +0530

    Made Media config consitent with Deployment

--- placementPortal/settings.py
+++ placementPortal/settings.py
@@ -152,10 +152,7 @@ LOGOUT_URL = "/api/auth/token/logout/"
 
 STATIC_URL = "/static/"
 
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "placementPortal/media")
-
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
