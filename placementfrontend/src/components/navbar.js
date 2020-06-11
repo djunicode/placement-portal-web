26805b756346230261e0da2e0175eea85176152c
SiddhiJhunjhunwala <siddhi2000jhun@gmail.com>
2020-06-11 20:32:59 +0530

    final auth

--- placementfrontend/src/components/navbar.js
+++ placementfrontend/src/components/navbar.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 function Nav(){
     return(
     <div className="container">
     <div className="col-lg-12">
-     <div className="Tpo2_topnav">
-   
-     <div className="col-12">  <Link to="/add" style={{ textDecoration: 'none' ,color:'#000' }} >RECRUITERS</Link></div>
-     <div className="col-12">  <Link to="/Tpo2" style={{ textDecoration: 'none',color:'#000' }} >STUDENTS</Link></div>
+     <div className="topnav">
+     <div className="col-12"><a href="#home">RECRUITERS</a></div>
+     <div className="col-12"><a className="active" href="#about">STUDENTS</a></div>
         <form className="form-group float-right ">
-                <input className="form-control Tpo2_inp2" type="text" placeholder="Search"/>
-                <button className="btn"><i className="fa fa-search Tpo2_glass2"></i></button>       
+                <input className="form-control inp2" type="text" placeholder="Search"/>
+                <button className="btn"><i className="fa fa-search glass2"></i></button>       
         </form>
     </div>
     </div>
